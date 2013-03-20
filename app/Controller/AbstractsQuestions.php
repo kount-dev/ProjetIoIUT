a21bf4eddbefc6aaeb931f93677ebbcbede58ecb
Flodess <florian.deschatres@gmail.com>
2013-03-20 13:51:04 +0100

    Ajout de la classe abstraite

--- app/Controller/AbstractsQuestions.php
+++ app/Controller/AbstractsQuestions.php
@@ -1,22 +1,30 @@
 <?php
-App::uses('AbstractsQuestions', 'Controller');
-
-class QcusController extends AbstractsQuestions {
-    public $component = array('Xml');
+/**
+* AbstractsQuestions Class
+*/
+class AbstractsQuestions
+{
+/**
+ *@desc Le constructeur de la classe, il n'y a rien à spécifier.
+ */
+    function __construct(argument)
+    {
+        //rien a specifier
+    }
 
 /**
  *@desc Cette fonction permet de charger, si besoin est, un fichier
  *@param array $param ('path'=>string)
  *@return true|false en fonction du succès ou non du chargement
  */
-    public function load($param);
+    public abstract function load($param);
 
 /**
  *@desc Cette fonction permet d'executer un module, elle doit retourner l'HTML a afficher
  *pour l'execution
  *@return le contenu HTML dans un string
  */
-    public function executeToHTML();
+    public abstract function executeToHTML();
 
 /**
  *@desc Cette fonction permet de créer un module, elle doit retourner l'HTML a afficher
@@ -24,7 +32,7 @@ class QcusController extends AbstractsQuestions {
  *@param string $urlRedirection : l'url sur laquelle devra pointer l'envoi du module
  *@return le contenu HTML dans un string
  */
-    public function creationToHTML($urlRedirection);
+    public abstract function creationToHTML($urlRedirection);
 
 
 /**
@@ -32,7 +40,7 @@ class QcusController extends AbstractsQuestions {
  *@param array $param ('reponses'=>array(), 'path'=>string)
  *@return boolean true | false en fonctio de s'il est bon
  */
-    public function valider($param);
+    public abstract function valider($param);
 
 /**
  *@desc cette fonction est celle qui gère l'ajout en base de données et création
@@ -40,11 +48,11 @@ class QcusController extends AbstractsQuestions {
  *@param array $param ('infos'=>array) (infos est en fait la variable POST)
  *@return boolean true|false en fonction du succès ou non de la sauvegarde
  */
-    public function saveFromPost($param);
+    public abstract function saveFromPost($param);
 
 /**
  *@desc Cette fonction va sauvegarder en base l'instance chargée
  */
-    public function saveInstance();
+    public abstract function saveInstance();
 }
 ?>
--- app/Controller/Component/StringComponent.php
+++ app/Controller/Component/StringComponent.php
@@ -4,16 +4,19 @@
 */
 class StringComponent extends Component
 {
+/**
+ *@desc Le constructeur de la classe, il n'y a rien à spécifier.
+ */
     function __construct(argument)
     {
         //rien a specifier
     }
 
-    /**
-    *@desc Cette fonction supprime les accents d'une chaine
-    *@param string la chaine a traiter
-    *@return la chaine modifiée
-    */
+/**
+ *@desc Cette fonction supprime les accents d'une chaine
+ *@param string la chaine a traiter
+ *@return la chaine modifiée
+ */
     function stripAccents($string){
         return strtr($string,'àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ',
     'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY');
