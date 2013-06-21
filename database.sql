ddacdfd3df13fcb9c2ac1654d303692d412d1dc7
Quentin MONTANT <montant.quentin@gmail.com>
2013-06-21 07:27:14 +0200

    Modif vdd et routes par défaut

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Jeu 20 Juin 2013 à 21:07
+-- Généré le: Ven 21 Juin 2013 à 07:26
 -- Version du serveur: 5.5.29
 -- Version de PHP: 5.4.10
 
@@ -11,7 +11,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Base de données: `bdd_ioiut`
+-- Base de données: `ioiut`
 --
 
 -- --------------------------------------------------------
@@ -30,7 +30,7 @@ CREATE TABLE `acos` (
   `lft` int(10) DEFAULT NULL,
   `rght` int(10) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=177 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=199 ;
 
 --
 -- Contenu de la table `acos`
@@ -39,149 +39,156 @@ CREATE TABLE `acos` (
 INSERT INTO `acos` (`id`, `parent_id`, `model`, `foreign_key`, `alias`, `lft`, `rght`) VALUES
 (1, NULL, NULL, NULL, 'controllers', 1, 2),
 (2, NULL, NULL, NULL, 'administrateur', 3, 4),
-(3, NULL, NULL, NULL, 'controllers', 5, 290),
+(3, NULL, NULL, NULL, 'controllers', 5, 304),
 (4, 3, NULL, NULL, 'Disciplines', 6, 17),
 (5, 4, NULL, NULL, 'index', 7, 8),
 (6, 4, NULL, NULL, 'view', 9, 10),
 (7, 4, NULL, NULL, 'add', 11, 12),
 (8, 4, NULL, NULL, 'edit', 13, 14),
 (9, 4, NULL, NULL, 'delete', 15, 16),
-(10, 3, NULL, NULL, 'Exercises', 18, 45),
+(10, 3, NULL, NULL, 'Exercises', 18, 41),
 (11, 10, NULL, NULL, 'index', 19, 20),
 (12, 10, NULL, NULL, 'view', 21, 22),
 (13, 10, NULL, NULL, 'add', 23, 24),
 (14, 10, NULL, NULL, 'edit', 25, 26),
 (15, 10, NULL, NULL, 'delete', 27, 28),
-(16, 3, NULL, NULL, 'GroupLists', 46, 57),
-(17, 16, NULL, NULL, 'index', 47, 48),
-(18, 16, NULL, NULL, 'view', 49, 50),
-(19, 16, NULL, NULL, 'add', 51, 52),
-(20, 16, NULL, NULL, 'edit', 53, 54),
-(21, 16, NULL, NULL, 'delete', 55, 56),
-(22, 3, NULL, NULL, 'Groups', 58, 69),
-(23, 22, NULL, NULL, 'index', 59, 60),
-(24, 22, NULL, NULL, 'view', 61, 62),
-(25, 22, NULL, NULL, 'add', 63, 64),
-(26, 22, NULL, NULL, 'edit', 65, 66),
-(27, 22, NULL, NULL, 'delete', 67, 68),
-(28, 3, NULL, NULL, 'IutGroups', 70, 81),
-(29, 28, NULL, NULL, 'index', 71, 72),
-(30, 28, NULL, NULL, 'view', 73, 74),
-(31, 28, NULL, NULL, 'add', 75, 76),
-(32, 28, NULL, NULL, 'edit', 77, 78),
-(33, 28, NULL, NULL, 'delete', 79, 80),
-(34, 3, NULL, NULL, 'Pages', 82, 85),
-(35, 34, NULL, NULL, 'display', 83, 84),
-(42, 3, NULL, NULL, 'Users', 86, 109),
-(43, 42, NULL, NULL, 'index', 87, 88),
-(44, 42, NULL, NULL, 'view', 89, 90),
-(45, 42, NULL, NULL, 'add', 91, 92),
-(46, 42, NULL, NULL, 'edit', 93, 94),
-(47, 42, NULL, NULL, 'delete', 95, 96),
-(48, 42, NULL, NULL, 'login', 97, 98),
-(49, 42, NULL, NULL, 'logout', 99, 100),
-(50, 3, NULL, NULL, 'AclExtras', 110, 111),
-(51, 3, NULL, NULL, 'DebugKit', 112, 119),
-(52, 51, NULL, NULL, 'ToolbarAccess', 113, 118),
-(53, 52, NULL, NULL, 'history_state', 114, 115),
-(54, 52, NULL, NULL, 'sql_explain', 116, 117),
-(61, 3, NULL, NULL, 'Questions', 120, 141),
-(62, 61, NULL, NULL, 'index', 121, 122),
-(63, 61, NULL, NULL, 'view', 123, 124),
-(64, 61, NULL, NULL, 'add', 125, 126),
-(65, 61, NULL, NULL, 'edit', 127, 128),
-(66, 61, NULL, NULL, 'delete', 129, 130),
+(16, 3, NULL, NULL, 'GroupLists', 42, 53),
+(17, 16, NULL, NULL, 'index', 43, 44),
+(18, 16, NULL, NULL, 'view', 45, 46),
+(19, 16, NULL, NULL, 'add', 47, 48),
+(20, 16, NULL, NULL, 'edit', 49, 50),
+(21, 16, NULL, NULL, 'delete', 51, 52),
+(22, 3, NULL, NULL, 'Groups', 54, 65),
+(23, 22, NULL, NULL, 'index', 55, 56),
+(24, 22, NULL, NULL, 'view', 57, 58),
+(25, 22, NULL, NULL, 'add', 59, 60),
+(26, 22, NULL, NULL, 'edit', 61, 62),
+(27, 22, NULL, NULL, 'delete', 63, 64),
+(28, 3, NULL, NULL, 'IutGroups', 66, 77),
+(29, 28, NULL, NULL, 'index', 67, 68),
+(30, 28, NULL, NULL, 'view', 69, 70),
+(31, 28, NULL, NULL, 'add', 71, 72),
+(32, 28, NULL, NULL, 'edit', 73, 74),
+(33, 28, NULL, NULL, 'delete', 75, 76),
+(34, 3, NULL, NULL, 'Pages', 78, 81),
+(35, 34, NULL, NULL, 'display', 79, 80),
+(42, 3, NULL, NULL, 'Users', 82, 105),
+(43, 42, NULL, NULL, 'index', 83, 84),
+(44, 42, NULL, NULL, 'view', 85, 86),
+(45, 42, NULL, NULL, 'add', 87, 88),
+(46, 42, NULL, NULL, 'edit', 89, 90),
+(47, 42, NULL, NULL, 'delete', 91, 92),
+(48, 42, NULL, NULL, 'login', 93, 94),
+(49, 42, NULL, NULL, 'logout', 95, 96),
+(50, 3, NULL, NULL, 'AclExtras', 106, 107),
+(51, 3, NULL, NULL, 'DebugKit', 108, 115),
+(52, 51, NULL, NULL, 'ToolbarAccess', 109, 114),
+(53, 52, NULL, NULL, 'history_state', 110, 111),
+(54, 52, NULL, NULL, 'sql_explain', 112, 113),
+(61, 3, NULL, NULL, 'Questions', 116, 137),
+(62, 61, NULL, NULL, 'index', 117, 118),
+(63, 61, NULL, NULL, 'view', 119, 120),
+(64, 61, NULL, NULL, 'add', 121, 122),
+(65, 61, NULL, NULL, 'edit', 123, 124),
+(66, 61, NULL, NULL, 'delete', 125, 126),
 (74, 10, NULL, NULL, 'load', 29, 30),
-(77, 10, NULL, NULL, 'valider', 31, 32),
-(78, 10, NULL, NULL, 'saveFromPost', 33, 34),
-(79, 10, NULL, NULL, 'saveInstance', 35, 36),
-(80, 3, NULL, NULL, 'ExercisesDisciplines', 142, 153),
-(81, 80, NULL, NULL, 'index', 143, 144),
-(82, 80, NULL, NULL, 'view', 145, 146),
-(83, 80, NULL, NULL, 'add', 147, 148),
-(84, 80, NULL, NULL, 'edit', 149, 150),
-(85, 80, NULL, NULL, 'delete', 151, 152),
-(86, 3, NULL, NULL, 'ExercisesQuestions', 154, 165),
-(87, 86, NULL, NULL, 'index', 155, 156),
-(88, 86, NULL, NULL, 'view', 157, 158),
-(89, 86, NULL, NULL, 'add', 159, 160),
-(90, 86, NULL, NULL, 'edit', 161, 162),
-(91, 86, NULL, NULL, 'delete', 163, 164),
-(92, 3, NULL, NULL, 'Qcus', 166, 209),
-(93, 92, NULL, NULL, 'index', 167, 168),
-(94, 92, NULL, NULL, 'load', 169, 170),
-(97, 92, NULL, NULL, 'valider', 171, 172),
-(98, 92, NULL, NULL, 'saveFromPost', 173, 174),
-(99, 92, NULL, NULL, 'saveInstance', 175, 176),
-(100, 92, NULL, NULL, 'view', 177, 178),
-(101, 92, NULL, NULL, 'add', 179, 180),
-(102, 92, NULL, NULL, 'edit', 181, 182),
-(103, 92, NULL, NULL, 'delete', 183, 184),
-(104, 3, NULL, NULL, 'QuestionTypes', 210, 221),
-(105, 104, NULL, NULL, 'index', 211, 212),
-(106, 104, NULL, NULL, 'view', 213, 214),
-(107, 104, NULL, NULL, 'add', 215, 216),
-(108, 104, NULL, NULL, 'edit', 217, 218),
-(109, 104, NULL, NULL, 'delete', 219, 220),
-(111, 3, NULL, NULL, 'QuestionsDisciplines', 222, 233),
-(112, 111, NULL, NULL, 'index', 223, 224),
-(113, 111, NULL, NULL, 'view', 225, 226),
-(114, 111, NULL, NULL, 'add', 227, 228),
-(115, 111, NULL, NULL, 'edit', 229, 230),
-(116, 111, NULL, NULL, 'delete', 231, 232),
-(124, 42, NULL, NULL, 'leaderboard', 101, 102),
-(125, 42, NULL, NULL, 'sortleaderboard', 103, 104),
-(126, 3, NULL, NULL, 'Answers', 234, 263),
-(127, 126, NULL, NULL, 'index', 235, 236),
-(128, 126, NULL, NULL, 'displayUserLog', 237, 238),
-(129, 126, NULL, NULL, 'displayByIdExercise', 239, 240),
-(130, 126, NULL, NULL, 'displayByIdUser', 241, 242),
-(131, 126, NULL, NULL, 'view', 243, 244),
-(132, 126, NULL, NULL, 'add', 245, 246),
-(133, 126, NULL, NULL, 'edit', 247, 248),
-(134, 126, NULL, NULL, 'delete', 249, 250),
-(135, 126, NULL, NULL, 'saveAnswer', 251, 252),
-(136, 126, NULL, NULL, 'generationXML', 253, 254),
-(137, 126, NULL, NULL, 'readXMLAnswer', 255, 256),
-(138, 126, NULL, NULL, 'feedback', 257, 258),
-(139, 126, NULL, NULL, 'successRate', 259, 260),
-(140, 126, NULL, NULL, 'scoreEnFonctionNbRealisations', 261, 262),
-(141, 10, NULL, NULL, 'displayXmlToHtml', 37, 38),
-(142, 10, NULL, NULL, 'import', 39, 40),
-(143, 10, NULL, NULL, 'listByUser', 41, 42),
-(144, 10, NULL, NULL, 'display', 43, 44),
-(145, 92, NULL, NULL, 'displayXmlToHtml', 185, 186),
-(146, 92, NULL, NULL, 'displayWithReponses', 187, 188),
-(147, 92, NULL, NULL, 'correction', 189, 190),
-(148, 92, NULL, NULL, 'saveQuestion', 191, 192),
-(149, 92, NULL, NULL, 'saveEditQuestion', 193, 194),
-(150, 92, NULL, NULL, 'addChoice', 195, 196),
-(151, 92, NULL, NULL, 'addEditChoice', 197, 198),
-(152, 92, NULL, NULL, 'generationXML', 199, 200),
-(153, 92, NULL, NULL, 'import', 201, 202),
-(154, 92, NULL, NULL, 'getQuestionType', 203, 204),
-(155, 92, NULL, NULL, 'checkDtdUpload', 205, 206),
-(156, 92, NULL, NULL, 'saveUploadQuestion', 207, 208),
-(157, 3, NULL, NULL, 'Qos', 264, 277),
-(158, 157, NULL, NULL, 'load', 265, 266),
-(159, 157, NULL, NULL, 'executeToHTML', 267, 268),
-(160, 157, NULL, NULL, 'add', 269, 270),
-(161, 157, NULL, NULL, 'valider', 271, 272),
-(162, 157, NULL, NULL, 'saveFromPost', 273, 274),
-(163, 157, NULL, NULL, 'saveInstance', 275, 276),
-(164, 61, NULL, NULL, 'import', 131, 132),
-(165, 61, NULL, NULL, 'getQuestionType', 133, 134),
-(166, 61, NULL, NULL, 'checkDtdUpload', 135, 136),
-(167, 61, NULL, NULL, 'saveUploadQuestion', 137, 138),
-(168, 61, NULL, NULL, 'saveQuestion', 139, 140),
-(169, 42, NULL, NULL, 'admin', 105, 106),
-(170, 42, NULL, NULL, 'initDB', 107, 108),
-(171, 3, NULL, NULL, 'ExerciseGroupLists', 278, 289),
-(172, 171, NULL, NULL, 'index', 279, 280),
-(173, 171, NULL, NULL, 'view', 281, 282),
-(174, 171, NULL, NULL, 'add', 283, 284),
-(175, 171, NULL, NULL, 'edit', 285, 286),
-(176, 171, NULL, NULL, 'delete', 287, 288);
+(80, 3, NULL, NULL, 'ExercisesDisciplines', 138, 149),
+(81, 80, NULL, NULL, 'index', 139, 140),
+(82, 80, NULL, NULL, 'view', 141, 142),
+(83, 80, NULL, NULL, 'add', 143, 144),
+(84, 80, NULL, NULL, 'edit', 145, 146),
+(85, 80, NULL, NULL, 'delete', 147, 148),
+(86, 3, NULL, NULL, 'ExercisesQuestions', 150, 161),
+(87, 86, NULL, NULL, 'index', 151, 152),
+(88, 86, NULL, NULL, 'view', 153, 154),
+(89, 86, NULL, NULL, 'add', 155, 156),
+(90, 86, NULL, NULL, 'edit', 157, 158),
+(91, 86, NULL, NULL, 'delete', 159, 160),
+(92, 3, NULL, NULL, 'Qcus', 162, 199),
+(93, 92, NULL, NULL, 'index', 163, 164),
+(94, 92, NULL, NULL, 'load', 165, 166),
+(100, 92, NULL, NULL, 'view', 167, 168),
+(101, 92, NULL, NULL, 'add', 169, 170),
+(102, 92, NULL, NULL, 'edit', 171, 172),
+(103, 92, NULL, NULL, 'delete', 173, 174),
+(104, 3, NULL, NULL, 'QuestionTypes', 200, 211),
+(105, 104, NULL, NULL, 'index', 201, 202),
+(106, 104, NULL, NULL, 'view', 203, 204),
+(107, 104, NULL, NULL, 'add', 205, 206),
+(108, 104, NULL, NULL, 'edit', 207, 208),
+(109, 104, NULL, NULL, 'delete', 209, 210),
+(111, 3, NULL, NULL, 'QuestionsDisciplines', 212, 223),
+(112, 111, NULL, NULL, 'index', 213, 214),
+(113, 111, NULL, NULL, 'view', 215, 216),
+(114, 111, NULL, NULL, 'add', 217, 218),
+(115, 111, NULL, NULL, 'edit', 219, 220),
+(116, 111, NULL, NULL, 'delete', 221, 222),
+(124, 42, NULL, NULL, 'leaderboard', 97, 98),
+(125, 42, NULL, NULL, 'sortleaderboard', 99, 100),
+(126, 3, NULL, NULL, 'Answers', 224, 253),
+(127, 126, NULL, NULL, 'index', 225, 226),
+(128, 126, NULL, NULL, 'displayUserLog', 227, 228),
+(129, 126, NULL, NULL, 'displayByIdExercise', 229, 230),
+(130, 126, NULL, NULL, 'displayByIdUser', 231, 232),
+(131, 126, NULL, NULL, 'view', 233, 234),
+(132, 126, NULL, NULL, 'add', 235, 236),
+(133, 126, NULL, NULL, 'edit', 237, 238),
+(134, 126, NULL, NULL, 'delete', 239, 240),
+(135, 126, NULL, NULL, 'saveAnswer', 241, 242),
+(136, 126, NULL, NULL, 'generationXML', 243, 244),
+(137, 126, NULL, NULL, 'readXMLAnswer', 245, 246),
+(138, 126, NULL, NULL, 'feedback', 247, 248),
+(139, 126, NULL, NULL, 'successRate', 249, 250),
+(140, 126, NULL, NULL, 'scoreEnFonctionNbRealisations', 251, 252),
+(141, 10, NULL, NULL, 'displayXmlToHtml', 31, 32),
+(142, 10, NULL, NULL, 'import', 33, 34),
+(143, 10, NULL, NULL, 'listByUser', 35, 36),
+(144, 10, NULL, NULL, 'display', 37, 38),
+(145, 92, NULL, NULL, 'displayXmlToHtml', 175, 176),
+(146, 92, NULL, NULL, 'displayWithReponses', 177, 178),
+(147, 92, NULL, NULL, 'correction', 179, 180),
+(148, 92, NULL, NULL, 'saveQuestion', 181, 182),
+(149, 92, NULL, NULL, 'saveEditQuestion', 183, 184),
+(150, 92, NULL, NULL, 'addChoice', 185, 186),
+(151, 92, NULL, NULL, 'addEditChoice', 187, 188),
+(152, 92, NULL, NULL, 'generationXML', 189, 190),
+(153, 92, NULL, NULL, 'import', 191, 192),
+(154, 92, NULL, NULL, 'getQuestionType', 193, 194),
+(155, 92, NULL, NULL, 'checkDtdUpload', 195, 196),
+(156, 92, NULL, NULL, 'saveUploadQuestion', 197, 198),
+(164, 61, NULL, NULL, 'import', 127, 128),
+(165, 61, NULL, NULL, 'getQuestionType', 129, 130),
+(166, 61, NULL, NULL, 'checkDtdUpload', 131, 132),
+(167, 61, NULL, NULL, 'saveUploadQuestion', 133, 134),
+(168, 61, NULL, NULL, 'saveQuestion', 135, 136),
+(169, 42, NULL, NULL, 'admin', 101, 102),
+(170, 42, NULL, NULL, 'initDB', 103, 104),
+(171, 3, NULL, NULL, 'ExerciseGroupLists', 254, 265),
+(172, 171, NULL, NULL, 'index', 255, 256),
+(173, 171, NULL, NULL, 'view', 257, 258),
+(174, 171, NULL, NULL, 'add', 259, 260),
+(175, 171, NULL, NULL, 'edit', 261, 262),
+(176, 171, NULL, NULL, 'delete', 263, 264),
+(179, 10, NULL, NULL, 'importDataXML', 39, 40),
+(180, 3, NULL, NULL, 'Qcms', 266, 303),
+(181, 180, NULL, NULL, 'index', 267, 268),
+(182, 180, NULL, NULL, 'load', 269, 270),
+(183, 180, NULL, NULL, 'displayXmlToHtml', 271, 272),
+(184, 180, NULL, NULL, 'displayWithReponses', 273, 274),
+(185, 180, NULL, NULL, 'correction', 275, 276),
+(186, 180, NULL, NULL, 'add', 277, 278),
+(187, 180, NULL, NULL, 'edit', 279, 280),
+(188, 180, NULL, NULL, 'saveQuestion', 281, 282),
+(189, 180, NULL, NULL, 'saveEditQuestion', 283, 284),
+(190, 180, NULL, NULL, 'addChoice', 285, 286),
+(191, 180, NULL, NULL, 'addEditChoice', 287, 288),
+(192, 180, NULL, NULL, 'generationXML', 289, 290),
+(193, 180, NULL, NULL, 'view', 291, 292),
+(194, 180, NULL, NULL, 'import', 293, 294),
+(195, 180, NULL, NULL, 'getQuestionType', 295, 296),
+(196, 180, NULL, NULL, 'checkDtdUpload', 297, 298),
+(197, 180, NULL, NULL, 'saveUploadQuestion', 299, 300),
+(198, 180, NULL, NULL, 'delete', 301, 302);
 
 -- --------------------------------------------------------
 
@@ -200,7 +207,7 @@ CREATE TABLE `answers` (
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=29 ;
 
 --
 -- Contenu de la table `answers`
@@ -209,7 +216,13 @@ CREATE TABLE `answers` (
 INSERT INTO `answers` (`id`, `namefile`, `user_id`, `exercise_id`, `attempt_number`, `success_rate`, `created`, `modified`) VALUES
 (16, '16_2013-06-09.xml', 11, 1, 12, 100, '2013-06-09 19:29:06', '2013-06-09 19:29:06'),
 (17, '17_2013-06-09.xml', 10, 1, 13, 100, '2013-06-09 19:29:36', '2013-06-09 19:29:36'),
-(18, '18_2013-06-09.xml', 11, 1, 3, 100, '2013-06-09 19:33:48', '2013-06-09 19:33:48');
+(18, '18_2013-06-09.xml', 11, 1, 3, 100, '2013-06-09 19:33:48', '2013-06-09 19:33:48'),
+(19, '19_2013-06-12.xml', 10, 1, 4, 100, '2013-06-12 09:11:31', '2013-06-12 09:11:31'),
+(20, '20_2013-06-12.xml', 9, 3, 5, 100, '2013-06-12 10:08:04', '2013-06-12 10:08:04'),
+(21, '21_2013-06-16.xml', 8, 1, 6, 100, '2013-06-16 08:39:36', '2013-06-16 08:39:36'),
+(22, '22_2013-06-16.xml', 8, 2, 7, 100, '2013-06-16 08:41:54', '2013-06-16 08:41:54'),
+(23, '23_2013-06-16.xml', 10, 3, 8, 100, '2013-06-16 08:58:39', '2013-06-16 08:58:39'),
+(24, '24_2013-06-16.xml', 9, 2, 9, 100, '2013-06-16 08:59:22', '2013-06-16 08:59:22');
 
 -- --------------------------------------------------------
 
@@ -508,14 +521,14 @@ CREATE TABLE `questions` (
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
 
 --
 -- Contenu de la table `questions`
 --
 
 INSERT INTO `questions` (`id`, `namefile`, `user_id`, `points`, `difficulty`, `question_type_id`, `created`, `modified`) VALUES
-(1, 'qcu_1_2013-06-07.xml', 4, 1, 3, 1, '2013-06-07 12:15:12', '2013-06-09 17:08:20'),
+(1, 'qcu_1_2013-06-07.xml', 4, 1, 3, 1, '2013-06-07 12:15:12', '2013-06-20 21:47:30'),
 (2, 'qcu_2_2013-06-07.xml', 4, 5, 3, 1, '2013-06-07 12:15:13', '2013-06-07 12:15:13'),
 (3, 'qcu_3_2013-06-07.xml', 4, 3, 3, 1, '2013-06-07 12:15:14', '2013-06-07 12:15:14'),
 (4, 'qcu_4_2013-06-07.xml', 4, 1, 3, 1, '2013-06-07 12:15:40', '2013-06-07 12:15:40'),
@@ -539,7 +552,7 @@ CREATE TABLE `questions_disciplines` (
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=33 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=30 ;
 
 --
 -- Contenu de la table `questions_disciplines`
@@ -563,9 +576,7 @@ INSERT INTO `questions_disciplines` (`id`, `question_id`, `discipline_id`, `crea
 (15, 8, 2, NULL, NULL),
 (16, 8, 4, NULL, NULL),
 (17, 9, 3, NULL, NULL),
-(18, 9, 4, NULL, NULL),
-(31, 17, 3, NULL, NULL),
-(32, 17, 4, NULL, NULL);
+(18, 9, 4, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -581,7 +592,7 @@ CREATE TABLE `question_types` (
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Contenu de la table `question_types`
@@ -589,7 +600,7 @@ CREATE TABLE `question_types` (
 
 INSERT INTO `question_types` (`id`, `name`, `controller`, `created`, `modified`) VALUES
 (1, 'Question Ã  choix unique', 'Qcu', '2013-03-22 15:23:03', '2013-03-22 15:23:03'),
-(2, 'Question ouverte', 'Qo', '2013-03-22 15:23:49', '2013-03-22 15:23:49');
+(3, 'Question Ã  choix multiple', 'Qcm', '2013-06-20 23:45:26', '2013-06-20 23:45:50');
 
 -- --------------------------------------------------------
 
@@ -619,8 +630,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `mail`, `xp`, `actual_rank`, `last_rank`, `group_id`, `avatar_namefile`, `created`, `modified`) VALUES
-(8, 'florian', '51ba318570e3d416974b93511b0024512bf5dcb4', 'florian@florian.fr', 0, NULL, NULL, 1, NULL, '2013-06-09 19:16:59', '2013-06-09 19:18:03'),
-(9, 'louis', 'b28d04149bc5151bfc258aa7c01f605d354f81ef', 'louis@louis.fr', 0, NULL, NULL, 1, NULL, '2013-06-09 19:17:38', '2013-06-09 19:17:38'),
-(10, 'quentin', 'aa26144053d99fcff013ff1e96c169d0f7a35401', 'quentin@quentin.fr', 6.03, 2, 1, 1, NULL, '2013-06-09 19:17:56', '2013-06-09 19:17:56'),
-(11, 'eleve', 'c7b15743ef49977b9164e2eb2bce1ced7bc1fcf0', 'eleve@eleve.fr', 10.08, 1, 2, 2, NULL, '2013-06-09 19:26:10', '2013-06-09 19:26:10'),
-(12, 'administrateur', 'caca30d5ff5ac02b71c2ea2c56f8d3c01dfd4fc0', 'admin@admin.fr', 0, NULL, NULL, 1, NULL, '2013-06-20 21:05:52', '2013-06-20 21:05:52');
+(8, 'florian', 'ef3ce76a84fe7519453a613804375f66b9a7a118', 'florian@florian.fr', 12.06, 2, 1, 1, NULL, '2013-06-09 19:16:59', '2013-06-12 09:38:08'),
+(9, 'louis', 'b28d04149bc5151bfc258aa7c01f605d354f81ef', 'louis@louis.fr', 12.06, 3, 4, 1, NULL, '2013-06-09 19:17:38', '2013-06-12 09:38:42'),
+(10, 'quentin', 'aa26144053d99fcff013ff1e96c169d0f7a35401', 'quentin@quentin.fr', 16.91, 1, 2, 1, NULL, '2013-06-09 19:17:56', '2013-06-09 19:17:56'),
+(11, 'eleve', 'c7b15743ef49977b9164e2eb2bce1ced7bc1fcf0', 'eleve@eleve.fr', 10.08, 4, 3, 2, NULL, '2013-06-09 19:26:10', '2013-06-09 19:26:10'),
+(12, 'administrateur', 'caca30d5ff5ac02b71c2ea2c56f8d3c01dfd4fc0', 'admin@admin.fr', 0, NULL, NULL, 1, NULL, '2013-06-21 07:26:13', '2013-06-21 07:26:13');
